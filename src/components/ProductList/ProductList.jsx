4f60c8c0514af0f9de204c798bc9a81854cda785
KayLin <kay94837@gmail.com>
2022-12-20 14:11:43 +0800

    update

---
+++ src/components/ProductList/ProductList.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function ProductList(props) {
+  const { products } = props;
+
+  return (
+    <div className="bg-white">
+      <div className="mx-auto max-w-2xl py-8 px-6 sm:py-12 sm:px-6 lg:max-w-7xl lg:px-8">
+        <div className="grid grid-cols-2 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+          {products.map((product) => (
+            <Link
+              to={`/shop/product/${product.id}`}
+              key={product.id}
+              className="group"
+            >
+              <div className="aspect-w-16 aspect-h-9 w-full overflow-hidden rounded-lg bg-gray-100">
+                <img
+                  src={product.attributes.imgSrc}
+                  className="h-full w-full object-cover object-center group-hover:opacity-75 hover:scale-105"
+                  alt={product.attributes.title}
+                />
+              </div>
+              <h3 className="mt-3 text-sm text-gray-700">{product.attributes.title}</h3>
+              <p className="mt-2 text-base font-medium text-gray-900">
+                NT$
+                {product.attributes.price.toLocaleString('en-US')}
+              </p>
+            </Link>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ProductList;
--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -4,6 +4,7 @@ import ProductFilter from '../ProductFilter/ProductFilter';
 
 import { useGetProductsQuery } from '../../store/api/productsApi';
 import Loading from '../UI/Loading/Loading';
+import ProductList from '../ProductList/ProductList';
 
 function Shop() {
   const { data: products, isSuccess, isLoading } = useGetProductsQuery();
@@ -54,32 +55,7 @@ function Shop() {
               onCategoryChange={changeCategoryHandler}
               onSortChange={changeSortHandler}
             />
-            <div className="bg-white">
-              <div className="mx-auto max-w-2xl py-8 px-4 sm:py-12 sm:px-6 lg:max-w-7xl lg:px-8">
-                <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-                  {filterProducts.map((product) => (
-                    <Link
-                      to={`/shop/product/${product.id}`}
-                      key={product.id}
-                      className="group"
-                    >
-                      <div className="aspect-w-16 aspect-h-9 w-full overflow-hidden rounded-lg bg-gray-100">
-                        <img
-                          src={product.attributes.imgSrc}
-                          className="h-full w-full object-cover object-center group-hover:opacity-75 hover:scale-105"
-                          alt={product.attributes.title}
-                        />
-                      </div>
-                      <h3 className="mt-3 text-sm text-gray-700">{product.attributes.title}</h3>
-                      <p className="mt-2 text-base font-medium text-gray-900">
-                        NT$
-                        {product.attributes.price.toLocaleString('en-US')}
-                      </p>
-                    </Link>
-                  ))}
-                </div>
-              </div>
-            </div>
+            <ProductList products={filterProducts} />
           </>
         )
       )}
