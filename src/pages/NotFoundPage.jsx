f790186ea59a710503c8efb1d9d50aad0e0f8790
KayLin <kay94837@gmail.com>
2023-04-12 00:57:27 +0800

    update text

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -5,7 +5,7 @@ export default function NotFoundPage() {
   return (
     <div className="mx-auto my-16 text-center ">
       <h2 className="text-darkslategray">404</h2>
-      <p className="my-4">抱歉，找不到您要的頁面</p>
+      <p className="my-4">該頁面不存在</p>
       <Link to="/" className="text-darkslategray font-bold">回到首頁</Link>
     </div>
   );
