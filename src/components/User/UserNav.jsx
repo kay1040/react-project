bfe2407cd9c26f9fa7d8c5d925f8041f4c57420a
KayLin <kay94837@gmail.com>
2023-04-18 00:21:43 +0800

    rename files

--- src/components/User/UserNav.jsx
+++ src/components/User/UserNav.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
-import { NavLink, Link, useNavigate } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faRightFromBracket } from '@fortawesome/free-solid-svg-icons';
 import styles from './UserNav.module.css';
@@ -8,7 +8,6 @@ import { logout } from '../../store/reducers/authSlice';
 
 export default function ProfileNav() {
   const dispatch = useDispatch();
-  const navigate = useNavigate();
   const active = {
     fontWeight: 'bold',
     color: 'darkslategray',
@@ -21,15 +20,15 @@ export default function ProfileNav() {
   return (
     <div className="mx-auto w-11/12 md:w-3/4">
       <ul className={styles.tabs}>
-        <li><NavLink to="/user/profile" style={changeStyle}>會員資料</NavLink></li>
-        <li><NavLink to="/user/order" style={changeStyle}>我的訂單</NavLink></li>
-        <li><NavLink to="/user/favorite" style={changeStyle}>我的收藏</NavLink></li>
+        <li><NavLink to="profile" style={changeStyle}>會員資料</NavLink></li>
+        <li><NavLink to="order" style={changeStyle}>我的訂單</NavLink></li>
+        <li><NavLink to="favorite" style={changeStyle}>我的收藏</NavLink></li>
         <li>
           <button
             type="button"
             onClick={() => { dispatch(logout()); }}
           >
-            <Link to={navigate(-1, { replace: true })}>
+            <Link to="/">
               <FontAwesomeIcon icon={faRightFromBracket} />
               &nbsp;登出
             </Link>
