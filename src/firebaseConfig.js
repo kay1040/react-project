fbbd18a4161173790a8092a801ebe766ce2f1783
KayLin <kay94837@gmail.com>
2023-04-21 13:14:35 +0800

    add .env

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,17 +1,19 @@
 import { initializeApp } from 'firebase/app';
 import { getAnalytics } from 'firebase/analytics';
 import { getFirestore } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyBiJ1U9lwerErAxL-CZXJ614G-PVQmU5Cs',
-  authDomain: 'my-firebase-c1f5e.firebaseapp.com',
-  projectId: 'my-firebase-c1f5e',
-  storageBucket: 'my-firebase-c1f5e.appspot.com',
-  messagingSenderId: '145855564666',
-  appId: '1:145855564666:web:a2733452a944305b48e4c3',
-  measurementId: 'G-KEKHSJ3NMS',
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
-export const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 export const analytics = getAnalytics(app);
 export const db = getFirestore(app);
+export const auth = getAuth(app);
