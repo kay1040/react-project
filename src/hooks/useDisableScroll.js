8f727d80e42f6b28d39a6b7e00c4825bb548195c
KayLin <kay94837@gmail.com>
2023-05-02 13:33:11 +0800

    optimize useDisableScroll.js

--- src/hooks/useDisableScroll.js
+++ src/hooks/useDisableScroll.js
@@ -1,19 +1,22 @@
 import { useEffect } from 'react';
 
 export default function useDisableScroll(disable) {
+  const scrollbarWidth = window.innerWidth - document.documentElement.clientWidth;
+  const count = document.querySelectorAll('.mask').length;
   useEffect(() => {
     const handleTouchMove = (e) => {
       e.preventDefault();
     };
     if (disable) {
       document.body.style.overflowY = 'hidden';
+      if (count < 1) document.body.style.paddingRight = `${scrollbarWidth}px`;
       document.body.addEventListener('touchmove', handleTouchMove, { passive: false });
     }
     return () => {
-      const count = document.querySelectorAll('.mask').length;
       // 若沒有遮罩層才顯示滾動條
       if (count < 1) {
         document.body.style.overflowY = 'auto';
+        document.body.style.paddingRight = 0;
       }
       document.body.removeEventListener('touchmove', handleTouchMove, { passive: false });
     };
