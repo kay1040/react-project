291e7faed7819da0cb78bb811e5c458652018a9a
KayLin <kay94837@gmail.com>
2023-05-07 13:12:46 +0800

    Fix CartPreview mobile scrolling issue

--- src/components/UI/Backdrop.jsx
+++ src/components/UI/Backdrop.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import useDisableScroll from '../../hooks/useDisableScroll'
+// import useDisableScroll from '../../hooks/useDisableScroll'
+import { ScrollLocky } from 'react-scroll-locky';
 
 const backdropRoot = document.getElementById('backdrop-root');
 
 export default function Backdrop(props) {
   // 禁止頁面滾動
-  useDisableScroll(true);
+  // useDisableScroll(true);
 
   return ReactDOM.createPortal(
-    <div {...props} className={`mask fixed inset-0 bg-black/[.3] z-[9999]`} />,
+    <ScrollLocky><div {...props} className={`mask fixed inset-0 bg-black/[.3] z-[9999]`} /></ScrollLocky>,
     backdropRoot,
   );
 }
